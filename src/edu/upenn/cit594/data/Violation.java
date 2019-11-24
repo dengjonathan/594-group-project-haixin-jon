5a51a26afad784cb385cfa93c51698a9bb35d8aa
Jonathan Deng <jonathandeng@Jonathans-MBP.attlocal.net>
2019-11-23 19:46:50 -0800

    scaffolding for parser and data classes

---
+++ src/edu/upenn/cit594/data/Violation.java
@@ -0,0 +1,56 @@
+package edu.upenn.cit594.data;
+
+import java.util.Date;
+
+/**
+ * Represents a parking violation read from the input source
+ */
+public class Violation {
+    public Violation(String id, Date date, int fine, String description, String vehicleId, String stateOfPlate, String zip) {
+        this.id = id;
+        this.date = date;
+        this.fine = fine;
+        this.description = description;
+        this.vehicleId = vehicleId;
+        this.stateOfPlate = stateOfPlate;
+        this.zip = zip;
+    }
+
+    // uuid for this violation
+    public String id;
+    private Date date;
+    private int fine;
+    private String description;
+    private String vehicleId;
+    // 2 letter abbreviation
+    private String stateOfPlate;
+    private String zip;
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getFine() {
+        return fine;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getStateOfPlate() {
+        return stateOfPlate;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+}
